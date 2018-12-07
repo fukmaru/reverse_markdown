38aa30a9a5ed985a5e0db032908844ed3815c655
fukmaru <k@fukmaru.com>
2018-12-07 16:08:43 +0900

    edit a, strong tag for slack mrkdwn_in.

--- lib/reverse_markdown/converters/a.rb
+++ lib/reverse_markdown/converters/a.rb
@@ -9,7 +9,7 @@ module ReverseMarkdown
         if href.to_s.start_with?('#') || href.to_s.empty? || name.empty?
           name
         else
-          link = "[#{name}](#{href}#{title})"
+          link = "<#{href}#{title}>"
           link.prepend(' ') if prepend_space?(node)
           link
         end
